9cb936b489fb770c140e063ed2a7a3dcc4af73a6
allain <allain.lalonde@gmail.com>
2015-01-22 12:04:15 -0500

    moving back into index.js, broke browserify

--- index.js
+++ index.js
@@ -111,5 +111,34 @@ var Store = function(name, endPoint) {
 
 inherits(Store, Readable);
 
-Store.Personal = require('./personal.js');
+Store.Personal = function(name, endPoint) {
+  var authHash = null;
+  var store = null;
 
+  var duplexStream = dynamicDuplex(function(auth, enc, cb) {
+		var newAuthHash = auth.auth && auth.profile ? auth.auth.network + '-' + auth.profile.id : null;
+	  if (authHash === newAuthHash) {
+			return cb(null, store);
+		}
+
+		if (store) {
+			//TODO: store.destroy()
+		}
+
+    if (auth) {
+			store = new Store('p-' + name + '-' + newAuthHash);
+			authHash = newAuthHash;
+			duplexStream.edit = store.edit.bind(store);
+			cb(null, store);
+		} else {
+			authHash = null;
+			store = null;
+      duplexStream.edit = function() {
+				debug('attempting to edit a disconnected personal stream');
+			};
+			cb(null, null);
+		}
+	});
+
+  return duplexStream;
+};
